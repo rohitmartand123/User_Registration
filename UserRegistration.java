651b9b294ae689a4ee77d046e11eb39c793c1ee3
rohitmartand123 <rohitmartand274@gmail.com>
2022-07-14 21:29:45 +0530

    Added: Check the email name is valid or not

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,48 +5,70 @@ import java.util.Scanner;
  *As a user need to enter valid first name
  */
 public class UserRegistration {
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //input first name
-        System.out.println("Enter first name");
-        String firstName = sc.next();
-        //calling checkFirstName method
-        checkFirstName(firstName);
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        //input first name
+	        System.out.println("Enter first name");
+	        String firstName = sc.next();
+	        //calling checkFirstName method
+	        checkFirstName(firstName);
 
-        //input last name
-        System.out.println("Enter last name");
-        String lastName = sc.next();
-        //calling check last name method
-        checkLastName(lastName);
-    }
+	        //input last name
+	        System.out.println("Enter last name");
+	        String lastName = sc.next();
+	        //calling check last name method
+	        checkLastName(lastName);
 
-    /*
-     *Entered pattern for first name
-     *Check the first name is valid or not
-     */
-    public static void checkFirstName(String firstName){
-        boolean isFirstName = firstName.matches("^[A-Z]{1}[a-z]{2,}$");
+	        //input email
+	        System.out.println("Enter email");
+	        String email = sc.next();
+	        //calling checkemail method
+	        checkEmail(email);
+	    }
 
-        if(isFirstName){
-            System.out.println(firstName + " " + "is valid first name");
-        }
-        else{
-            System.out.println(firstName + " " + "is not valid first name");
-        }
-    }
+	    /*
+	     *Entered pattern for first name
+	     *Check the first name is valid or not
+	     */
+	    public static void checkFirstName(String firstName){
+	        boolean isFirstName = firstName.matches("^[A-Z]{1}[a-z]{2,}$");
 
-    /*
-     *Entered pattern for last name
-     * And Check last name is valid or not
-     */
-    public static void checkLastName(String lastName){
-        boolean isLastName = lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+	        if(isFirstName){
+	            System.out.println(firstName + " " + "is valid first name");
+	        }
+	        else{
+	            System.out.println(firstName + " " + "is not valid first name");
+	        }
+	    }
 
-        if(isLastName){
-            System.out.println(lastName + " " + "is valid last name");
-        }
-        else{
-            System.out.println(lastName + " " + "is not valid last name");
-        }
-    }
+	    /*
+	     *Entered pattern for last name
+	     * And Check last name is valid or not
+	     */
+	    public static void checkLastName(String lastName){
+	        boolean isLastName = lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+
+	        if(isLastName){
+	            System.out.println(lastName + " " + "is valid last name");
+	        }
+	        else{
+	            System.out.println(lastName + " " + "is not valid last name");
+	        }
+	    }
+
+	    /*
+	     *Entered pattern for email
+	     * Check email is valid or not
+	     */
+	    public static void checkEmail(String email){
+	        boolean isEmail = email.matches("^[a-zA-z]+[.][a-zA-Z0-9]*@[a-z]{2}[.][a-z]{2}[.][a-z]*");
+
+	        if(isEmail){
+	            System.out.println(email + " " + "is valid email");
+	        }
+	        else{
+	            System.out.println(email + " " + "is not valid email");
+	        }
+	    }
 }
+	    
